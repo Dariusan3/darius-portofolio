50fa36f23bba5f9fbe2ee92219c07f5e4fd38944
osadici.darius <darius.osadici@yahoo.ro>
2025-04-26 00:20:42 +0300

    Created the Home section of the Portofolio website

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,22 @@
 const config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        lightHover: '#fcf4ff',
+        darkHover: "#2a004a",
+        darkTheme: '#11001F'
+      },
+      fontFamily: {
+        Outfit: ['var(--font-outfit)', 'sans-serif'],
+        Ovo: ['var(--font-ovo)', 'serif'],
+      },
+    }
+  },
   plugins: ["@tailwindcss/postcss"],
 };
 
